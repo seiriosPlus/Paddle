be4b0da8b9dcbcb661b12101605f79eb4c800fc6
tangwei12 <tangwei12@baidu.com>
2018-07-16 16:04:25 +0800

    debug optimize

--- paddle/fluid/operators/debug/print.h
+++ paddle/fluid/operators/debug/print.h
@@ -119,9 +119,47 @@ struct Formater {
 
 void PrintVariableLod(const framework::Scope& scope,
                       const std::string& var_name, const std::string& message,
+                      const bool print_tensor_name,
                       const bool print_tensor_type,
                       const bool print_tensor_shape,
-                      const bool print_tensor_lod, const int summarize);
+                      const bool print_tensor_lod, const int summarize) {
+  auto* in_var_ptr = scope.FindVar(var_name);
+  PADDLE_ENFORCE_NOT_NULL(in_var_ptr);
+  auto& in_tensor = in_var_ptr->Get<framework::LoDTensor>();
+
+  framework::LoDTensor printed_tensor;
+  printed_tensor.set_lod(in_tensor.lod());
+  printed_tensor.Resize(in_tensor.dims());
+
+  if (platform::is_cpu_place(in_tensor.place())) {
+    printed_tensor.ShareDataWith(in_tensor);
+  } else {
+    // copy data to cpu to print
+    platform::CPUPlace place;
+    framework::TensorCopy(in_tensor, place, &printed_tensor);
+  }
+
+  Formater formater;
+  formater.message = message;
+  if (print_tensor_name) {
+    formater.name = var_name;
+  }
+  if (print_tensor_type) {
+    formater.dtype = printed_tensor.type();
+  }
+  if (print_tensor_shape) {
+    auto& dims = printed_tensor.dims();
+    formater.dims.resize(dims.size());
+    for (int i = 0; i < dims.size(); ++i) formater.dims[i] = dims[i];
+  }
+  if (print_tensor_lod) {
+    formater.lod = printed_tensor.lod();
+  }
+  formater.summarize = summarize;
+  formater.data = reinterpret_cast<void*>(printed_tensor.data<void>());
+  formater(printed_tensor.numel());
+}
+
 }  // namespace debug
 }  // namespace operators
 }  // namespace paddle
